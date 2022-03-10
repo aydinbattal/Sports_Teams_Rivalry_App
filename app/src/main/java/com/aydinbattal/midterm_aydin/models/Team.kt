b7b87da83bf295b13100a309304ebfa53f2174d8
Aydin Battal <abattal35@gmail.com>
2022-03-10 18:04:40 -0500

    Implemented Team class.

---
+++ app/src/main/java/com/aydinbattal/midterm_aydin/models/Team.kt
@@ -0,0 +1,8 @@
+package com.aydinbattal.midterm_aydin.models
+
+/**
+ * Midterm-Aydin created by aydin
+ * student ID : 991521740
+ * on 2022-03-10 */
+data class Team(val name:String, val score:Int) {
+}
