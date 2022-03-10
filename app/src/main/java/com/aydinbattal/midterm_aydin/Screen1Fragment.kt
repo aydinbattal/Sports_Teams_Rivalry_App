93fcdcabdd63b9daaa09d3dd91398385c52df0ce
Aydin Battal <abattal35@gmail.com>
2022-03-10 14:07:55 -0500

    Implemented screen 1 UI.

---
+++ app/src/main/java/com/aydinbattal/midterm_aydin/Screen1Fragment.kt
@@ -0,0 +1,13 @@
+package com.aydinbattal.midterm_aydin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Screen1Fragment : Fragment(R.layout.fragment_screen1) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
