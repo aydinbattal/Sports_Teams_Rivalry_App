ddc459395012b28a75404b30b67a0f9fceb1ae5a
Aydin Battal <abattal35@gmail.com>
2022-03-10 14:19:00 -0500

    Implemented screen 2 UI.

---
+++ app/src/main/java/com/aydinbattal/midterm_aydin/Screen2Fragment.kt
@@ -0,0 +1,13 @@
+package com.aydinbattal.midterm_aydin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Screen2Fragment : Fragment(R.layout.fragment_screen2) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
